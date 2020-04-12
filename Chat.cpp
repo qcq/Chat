23332e91803fc8243e4c216a406328f0b8c7f7a5
qcq <qinchuanqing918@163.com>
2020-04-12 12:28:54 +0800

    initial commits with the cmake files and scrchs

---
+++ Chat.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "ChatConfig.h"
+
+using namespace std;
+
+int main() {
+    std::cout << "hello , this is the first step to make a mircle." << std::endl;
+    std::cout << "major:" << Chat_VERSION_MAJOR << " minor:" << Chat_VERSION_MINOR << std::endl;
+    return 0;
+}
