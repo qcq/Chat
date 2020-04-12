3043a312a02346dd1f511626f4b89e21a558db6e
qcq <qinchuanqing918@163.com>
2020-04-12 20:14:55 +0800

    prepare the framework of the Chat program

--- src/Chat.cpp
+++ src/Chat.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <boost/optional.hpp>
-#include <boost/optional/optional_io.hpp>
 
 #include "src/ChatConfig.h"
 
-    int main() {
+int main() {
     std::cout << "hello , this is the first step to make a mircle." << std::endl;
     std::cout << "major:" << Chat_VERSION_MAJOR << " minor:" << Chat_VERSION_MINOR << std::endl;
-    std::cout << boost::optional<int>(6) << std::endl;
+    auto opt = boost::optional<int>(6);
+    if (opt) {
+        std::cout << opt.get() << std::endl;
+    }
     return 0;
 }
